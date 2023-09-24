3e2c8bfd9f5c885e0087ead1e473cda0bb101093
reid23 <reid.dye@gmail.com>
2023-09-24 16:38:32 -0700

    midpoint

--- graphslam_variants.py
+++ graphslam_variants.py
@@ -131,7 +131,103 @@ class PolarGraphSLAM:
         new_lmhat.resize(len(self.lmhat), 2)
         self.lmhat = list(new_lmhat)
         return list(new_xhat), list(new_lmhat)
+
+class LinearGraphSLAM:
+    def __init__(self, **settings):
+        self.x0 = np.array([0,0])
+
+
+        self.firstupdate = True
+        self.landmarkTolerance = 2
+
+        self.Q = lambda n: DM_eye(n)*5
+        self.R = lambda n: DM_eye(n)
+
+        self.__dict__.update(settings) # sketchy but nobody's hacking us
+
+        # these things are after since they shouldn't be settings
+        self.nx = 1
+        self.nm = 0
+        self.A = np.array([[1,0],[0,1]])
+        self.b = np.copy(self.x0)
+        self.xhat = self.x0
+        self.lmhat = np.array([])
+
+    def update_graph(self, dx, z):
+        """updates graph given odo and lm measurements
+
+        Args:
+            dx (ndarray): vector of shape (2, 1) describing the estimated change in car location since the previous update 
+            z (ndarray): vector of shape (m, 2) describing locations of m landmarks relative to the car
+        """
+        self.xhat += dx
+        l = self.A.shape[1]
+
+        self.A = np.append(self.A, np.array([
+            [0]*(self.nx*2 - 2) + [-1, 0, 1] + [0]*(l - self.nx*2 - 1), # add equation x_k - x_(k-1) = dx_k
+            [0]*(self.nx*2 - 1) + [-1, 0, 1] + [0]*(l - self.nx*2 - 2), # add equation y_k - y_(k-1) = dy_k
+        ]), axis=0)
+        self.b = np.append(self.b, dx) # add the rhs of the above equations
+
+        # now deal with measurements
+        self.b = np.append(self.b, z+self.xhat, axis=0)
+
+
+        if self.firstupdate:
+            self.firstupdate = False
+            self.lmhat = z+self.xhat
+
+            pre_dims = self.A.shape
+            self.A = np.append(self.A, np.zeros((pre_dims[0]), len(z)), axis=1) # append all the landmark columns
+            self.A = np.append(self.A, np.zeros((len(z), self.A.shape[1])), axis=0)
+            for i in range(len(z)):
+                self.A[pre_dims[0]+(i*2),   pre_dims[1]+(i*2)] = 1 # add the 1 for the x coord of the landmark
+                self.A[pre_dims[0]+(i*2)+1, pre_dims[1]+(i*2)+1] = 1 # add the 1 for the y coord of the landmark
+                self.A[pre_dims[0]+(i*2),   pre_dims[1]-2] = 1 # add the 1 for the x coord of cur pos
+                self.A[pre_dims[0]+(i*2)+1, pre_dims[1]-1] = 1 # add the 1 for the y coord of cur pos
+            return
+
+        for i in z+self.xhat:
+            idx = np.argmin(dists:=norm(self.lmhat-i, axis=1))
+            if dists[idx] > self.landmarkTolerance:
+                idx = len(self.lmhat)
+                self.lmhat = np.concatenate((self.lmhat, i[np.newaxis]), axis=0)
+                self.lm.append(MX.sym(f'lm{idx}', 2))
+            self.lm_edges.append((self.x[-1] + DM(i) - self.lm[idx])) # x + z_i = lm_i
     
+    def solve_graph(self):
+        """solves the graph and updates everything
+        """
+        x_e = vertcat(*self.x_edges)
+        lm_e = vertcat(*self.lm_edges)
+        x0_e = self.x[0] - DM(self.x0)
+        qp = {
+            'x':vertcat(*self.x, *self.lm), 
+            'f': (x_e.T@self.Q(x_e.shape[0])@x_e 
+                + lm_e.T@self.R(lm_e.shape[0])@lm_e 
+                + x0_e.T@x0_e)
+        }
+        # solver = qpsol('solver', 'qpoases', qp, {'printLevel':'none', 'print_time':0, 'print_problem':0})
+        # solver = qpsol('solver', 'osqp', qp, {'print_time':0, 'print_problem':0})
+        solver = nlpsol('solver', 'ipopt', qp, self.solver_opts)
+
+        # actually solve the QP problem
+        soln = np.array(solver(x0=vertcat(*self.xhat, *self.lmhat))['x'])
+        split = len(self.xhat)*2
+
+        s = (len(self.xhat), 2)
+        x_guesses = np.array(soln[:split])
+        x_guesses.resize(s)
+
+        s = (len(self.lmhat), 2)
+        lm_guesses = np.array(soln[split:])
+        lm_guesses.resize(s)
+
+        self.xhat[:x_guesses.shape[0]] = list(x_guesses)
+        self.lmhat[:lm_guesses.shape[0]] = list(lm_guesses)
+        
+        return list(x_guesses), list(lm_guesses)
+
 if __name__ == '__main__':
     from graphslam_clean import Sim, get_path
     import matplotlib.pyplot as plt
